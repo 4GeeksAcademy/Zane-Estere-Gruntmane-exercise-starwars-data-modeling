85e579d309601ba9141c94cbf944076c14da0146
Zane Estere Gruntmane <141255754+esterzane@users.noreply.github.com>
2023-12-18 19:58:42 +0000

    starwars data modeling

--- src/models.py
+++ src/models.py
@@ -7,23 +7,45 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User (Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False)
+    first_name= Column (String (250), nullable=False)
+    last_name = Column (String (250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Favorits(Base):
+    __tablename__ = 'favorite'
+    id = Column(Integer, primary_key=True)
+    fav_planets = Column(Integer, ForeignKey('planets.id'))
+    fav_people = Column(Integer, ForeignKey('people.id'))
+    fav_starships = Column(Integer, ForeignKey('starships.id'))
+
+class People(Base): 
+    __tablename__ = 'People'
+    id = Column(Integer, primary_key=True)
+    name = Column(String())
+    user_id = Column(Integer, ForeignKey('user.id'))
+
+
+class Starships (Base): 
+    __tablename__ = 'Starships'
+    id = Column(Integer, primary_key=True)
+    name = Column (String()) 
+    model = Column (String())
+    birth_year = Column (Integer)
+    
+
+class Planets(Base):
+    __tablename__ = 'Planets'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     street_name = Column(String(250))
     street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+
 
     def to_dict(self):
         return {}
